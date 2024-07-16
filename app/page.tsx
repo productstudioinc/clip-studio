3cc60dd44d04b17efe37ef35be84e99c98cd9efc
Dillion Verma <hello@dillion.io>
2024-07-15 17:01:37 -0700

    fix: update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      landing page placeholder
+    <main className="container relative mx-auto p-4 md:p-16 gap-4 flex flex-col max-w-2xl lowercase">
+      <section>
+        <div className="flex items-center justify-between">
+          <div className="flex-1 space-y-1.5">
+            <h1 className="text-2xl font-bold">Clip Studio</h1>
+            <p className="max-w-md text-pretty text-sm text-muted-foreground">
+              Create viral social media content using AI
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
