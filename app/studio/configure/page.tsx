222918e816093156ace1f87537d00f41b33794cf
gursheyss <rkwarya@gmail.com>
2024-07-18 14:38:10 -0700

    move loading state to loading.tsx

--- app/studio/configure/page.tsx
+++ app/studio/configure/page.tsx
@@ -1,37 +1,7 @@
 import { getBackgrounds } from "@/utils/actions/getData";
 import { BackgroundSelect } from "./background-select";
-import { Suspense } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Card, CardContent } from "@/components/ui/card";
 
-const BackgroundSelectWrapper = async () => {
+export default async function ConfigurePage() {
   const backgrounds = await getBackgrounds();
   return <BackgroundSelect backgrounds={backgrounds} />;
-};
-
-export default function ConfigurePage() {
-  return (
-    <Suspense fallback={<BackgroundSelectSkeleton />}>
-      <BackgroundSelectWrapper />
-    </Suspense>
-  );
 }
-
-const BackgroundSelectSkeleton: React.FC = () => {
-  return (
-    <>
-      <Skeleton className="h-8 w-64 mb-6" />
-      <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
-        {[...Array(6)].map((_, index) => (
-          <Card key={index} className="h-full">
-            <CardContent className="p-1">
-              <div className="relative w-full pt-[56.25%]">
-                <Skeleton className="absolute inset-0 w-full h-full rounded-lg" />
-              </div>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-    </>
-  );
-};
--- app/studio/templates/page.tsx
+++ app/studio/templates/page.tsx
@@ -1,38 +1,8 @@
 import { getBackgrounds, getTemplates } from "@/utils/actions/getData";
 import { TemplateSelect } from "./template-select";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Suspense } from "react";
 
-const TemplateSelectWrapper = async () => {
+export default async function TemplatesPage() {
   const templates = await getTemplates();
   const backgrounds = await getBackgrounds();
   return <TemplateSelect templates={templates} />;
-};
-
-export default function TemplatesPage() {
-  return (
-    <Suspense fallback={<TemplateSelectSkeleton />}>
-      <TemplateSelectWrapper />
-    </Suspense>
-  );
 }
-
-const TemplateSelectSkeleton: React.FC = () => {
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
-      {[...Array(6)].map((_, index) => (
-        <Card key={index} className="h-full">
-          <CardHeader className="p-4">
-            <Skeleton className="h-6 w-3/4" />
-          </CardHeader>
-          <CardContent className="flex items-center justify-center pb-4 px-4">
-            <div className="relative w-full pt-[60%]">
-              <Skeleton className="absolute inset-0 w-full h-full rounded-lg" />
-            </div>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
-  );
-};
