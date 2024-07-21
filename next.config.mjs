7abd6dbd624160cd33e6d9ec1018534ed08ff824
gursheyss <rkwarya@gmail.com>
2024-07-21 13:10:30 -0700

    add posthog

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,19 @@ const nextConfig = {
       { source: "/studio", destination: "/studio/templates", permanent: true },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default withContentCollections(nextConfig);
