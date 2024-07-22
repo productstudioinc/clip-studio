5dc8d7c84845f656ee4701a4a1258fefce766a03
gursheyss <rkwarya@gmail.com>
2024-07-22 16:15:14 -0700

    fix posthog pageview and add highlight

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { GeistSans } from "geist/font/sans";
 import { PHProvider } from "./provider";
 import dynamic from "next/dynamic";
+import { HighlightInit } from "@highlight-run/next/client";
 
 const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
   ssr: false,
@@ -20,15 +21,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <PHProvider>
-        <body className={GeistSans.className}>
-          <ThemeProvider attribute="class" defaultTheme="light">
-            {children}
-            <Toaster position="top-right" />
-          </ThemeProvider>
-        </body>
-      </PHProvider>
-    </html>
+    <>
+      <HighlightInit
+        projectId={"memow6xd"}
+        serviceName="clip-studio"
+        tracingOrigins
+        networkRecording={{
+          enabled: true,
+          recordHeadersAndBody: true,
+          urlBlocklist: [],
+        }}
+      />
+      <html lang="en">
+        <PHProvider>
+          <body className={GeistSans.className}>
+            <PostHogPageView />
+            <ThemeProvider attribute="class" defaultTheme="light">
+              {children}
+              <Toaster position="top-right" />
+            </ThemeProvider>
+          </body>
+        </PHProvider>
+      </html>
+    </>
   );
 }
