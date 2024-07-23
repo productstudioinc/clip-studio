ccc32028ca8f5aef383c067aaa39755072541c68
gursheyss <rkwarya@gmail.com>
2024-07-22 17:48:12 -0700

    chore: Refactor getData.ts to use cached templates and backgrounds

--- utils/actions/getData.ts
+++ utils/actions/getData.ts
@@ -2,17 +2,30 @@
 
 import { db } from "@/db";
 import { templates } from "@/db/schema";
+import { unstable_cache } from "next/cache";
 
-export const getTemplates = async () => {
-  const response = await db.select().from(templates);
-  return response;
-};
+const getCachedTemplates = unstable_cache(
+  async () => {
+    const response = await db.select().from(templates);
+    return response;
+  },
+  ["templates"],
+  { revalidate: 86400 }
+);
 
-export const getBackgrounds = async () => {
-  const result = await db.query.backgrounds.findMany({
-    with: {
-      backgroundParts: true,
-    },
-  });
-  return result;
-};
+const getCachedBackgrounds = unstable_cache(
+  async () => {
+    const result = await db.query.backgrounds.findMany({
+      with: {
+        backgroundParts: true,
+      },
+    });
+    return result;
+  },
+  ["backgrounds"],
+  { revalidate: 86400 }
+);
+
+export const getTemplates = getCachedTemplates;
+
+export const getBackgrounds = getCachedBackgrounds;
