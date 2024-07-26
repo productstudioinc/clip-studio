e7fc9f89121a65121c875655818db8db4644bb8f
gursheyss <rkwarya@gmail.com>
2024-07-25 20:04:34 -0700

    change login

--- app/(app_external)/login/LoginComponent.tsx
+++ app/(app_external)/login/LoginComponent.tsx
@@ -1,6 +1,6 @@
 'use client';
-
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { createClient } from '@/supabase/clients';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -17,28 +17,37 @@ export default function LoginComponent() {
 				router.refresh();
 			}
 		});
-
 		return () => subscription.unsubscribe();
 	});
 
 	return (
-		<main className="flex flex-col">
-			<Button
-				variant="outline"
-				className="w-full"
-				onClick={() => {
-					supabase.auth.signInWithOAuth({
-						provider: 'google',
-						options: {
-							redirectTo: `${location.origin}/auth/callback`
-						}
-					});
-				}}
-			>
-				<GoogleIcon className="mr-2 h-4 w-4" />
-				Login with Google
-			</Button>
-		</main>
+		<div className="flex items-center justify-center min-h-screen pb-48">
+			<Card className="w-full max-w-sm">
+				<CardHeader>
+					<CardTitle className="text-2xl">Login</CardTitle>
+					<CardDescription>Login through one of our supported providers</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<div className="grid gap-4">
+						<Button
+							variant="outline"
+							className="w-full"
+							onClick={() => {
+								supabase.auth.signInWithOAuth({
+									provider: 'google',
+									options: {
+										redirectTo: `${location.origin}/auth/callback`
+									}
+								});
+							}}
+						>
+							<GoogleIcon className="mr-2 h-4 w-4" />
+							Login with Google
+						</Button>
+					</div>
+				</CardContent>
+			</Card>
+		</div>
 	);
 }
 
