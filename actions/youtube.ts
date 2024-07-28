7bb44f507ec2335b2ba4aa089c4f6b45182428b6
Dillion Verma <hello@dillion.io>
2024-07-28 16:50:19 +0200

    fix: add redirect uri

--- actions/youtube.ts
+++ actions/youtube.ts
@@ -28,6 +28,7 @@ export const connectYoutubeAccount = async () => {
 	const authUrl = youtubeAuthClient.generateAuthUrl({
 		access_type: 'offline',
 		scope: scopes,
+		redirect_uri: process.env.YOUTUBE_REDIRECT_URI,
 		include_granted_scopes: true,
 		state: state,
 		prompt: 'consent' // Forces consent screen to appear — necessary b/c google only issues refresh on initial auth
