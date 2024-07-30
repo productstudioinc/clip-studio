822cb6c6c9e1eed2dc96316f91ce77e07514876d
gursheyss <rkwarya@gmail.com>
2024-07-29 23:25:52 -0700

    feat: Improve responsiveness of account page

--- app/(app_protected_nopreview)/account/page.tsx
+++ app/(app_protected_nopreview)/account/page.tsx
@@ -30,106 +30,111 @@ export default async function Account() {
 				<div className="mx-auto grid w-full max-w-6xl gap-2">
 					<h1 className="text-3xl font-semibold">Settings</h1>
 				</div>
-				<div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-					<nav className="grid gap-4 text-sm text-muted-foreground">
-						<Link href="#" className="font-semibold text-primary">
-							General
-						</Link>
+				<div className="mx-auto grid w-full max-w-6xl items-start gap-6">
+					<nav className="grid gap-4 text-sm text-muted-foreground xl:hidden">
+						<h2 className="font-semibold text-primary text-xl">General</h2>
 					</nav>
-					<div className="grid gap-6">
-						<ProfileForm user={user} />
-						<Separator />
-						<div className="overflow-x-auto">
-							<div className="flex gap-4 pb-4">
-								<Card className="border-dashed min-h-[200px] flex flex-col text-balance text-center w-[250px] flex-shrink-0">
-									<CardHeader>
-										<CardTitle>Connect YouTube</CardTitle>
-									</CardHeader>
-									<CardContent className="flex flex-1">
-										<form action={connectYoutubeAccount} className="w-full flex items-end">
-											<Button size="sm" className="w-full">
-												<Icons.youtube className="size-4 mr-2" />
-												Connect Youtube
-											</Button>
-										</form>
-									</CardContent>
-								</Card>
-								{youtubeChannels.map((channel) => (
-									<Card
-										key={channel.id}
-										className="justify-center min-h-[200px] text-center w-[250px] flex-shrink-0"
-									>
+					<div className="grid xl:grid-cols-[250px_1fr] gap-6">
+						<nav className="hidden xl:grid gap-4 text-sm text-muted-foreground">
+							<Link href="#" className="font-semibold text-primary text-xl">
+								General
+							</Link>
+						</nav>
+						<div className="grid gap-6">
+							<ProfileForm user={user} />
+							<Separator />
+							<div className="overflow-x-auto">
+								<div className="flex gap-4 pb-4">
+									<Card className="border-dashed min-h-[200px] flex flex-col text-balance text-center w-[250px] flex-shrink-0">
 										<CardHeader>
-											<CardTitle>{channel.channelCustomUrl}</CardTitle>
+											<CardTitle>Connect YouTube</CardTitle>
 										</CardHeader>
-										<CardContent>
-											<div className="flex items-center space-x-4 justify-center">
-												<Avatar className="size-20">
-													<AvatarImage
-														src={channel.profile_picture_path as string}
-														alt={`${channel.channelCustomUrl} profile`}
-													/>
-													<AvatarFallback>{channel.channelCustomUrl[0]}</AvatarFallback>
-												</Avatar>
-												<div>
-													{channel.error && <p className="text-red-500">{channel.error}</p>}
-												</div>
-											</div>
+										<CardContent className="flex flex-1">
+											<form action={connectYoutubeAccount} className="w-full flex items-end">
+												<Button size="sm" className="w-full">
+													<Icons.youtube className="size-4 mr-2" />
+													Connect Youtube
+												</Button>
+											</form>
 										</CardContent>
-										<CardFooter>
-											<Button variant="destructive" size="sm" className="w-full">
-												Disconnect
-											</Button>
-										</CardFooter>
 									</Card>
-								))}
+									{youtubeChannels.map((channel) => (
+										<Card
+											key={channel.id}
+											className="justify-center min-h-[200px] text-center w-[250px] flex-shrink-0"
+										>
+											<CardHeader>
+												<CardTitle>{channel.channelCustomUrl}</CardTitle>
+											</CardHeader>
+											<CardContent>
+												<div className="flex items-center space-x-4 justify-center">
+													<Avatar className="size-20">
+														<AvatarImage
+															src={channel.profile_picture_path as string}
+															alt={`${channel.channelCustomUrl} profile`}
+														/>
+														<AvatarFallback>{channel.channelCustomUrl[0]}</AvatarFallback>
+													</Avatar>
+													<div>
+														{channel.error && <p className="text-red-500">{channel.error}</p>}
+													</div>
+												</div>
+											</CardContent>
+											<CardFooter>
+												<Button variant="destructive" size="sm" className="w-full">
+													Disconnect
+												</Button>
+											</CardFooter>
+										</Card>
+									))}
+								</div>
 							</div>
-						</div>
-						<Separator />
-						<div className="overflow-x-auto">
-							<div className="flex gap-4 pb-4">
-								<Card className="border-dashed min-h-[200px] flex flex-col text-balance text-center w-[250px] flex-shrink-0">
-									<CardHeader>
-										<CardTitle>Connect TikTok</CardTitle>
-									</CardHeader>
-									<CardContent className="flex flex-1">
-										<form action={connectTiktokAccount} className="w-full flex items-end">
-											<Button size="sm" className="w-full">
-												<Icons.tiktok className="size-4 mr-2" />
-												Connect TikTok
-											</Button>
-										</form>
-									</CardContent>
-								</Card>
-								{tiktokChannels.map((channel) => (
-									<Card
-										key={channel.id}
-										className="justify-center min-h-[200px] text-center w-[250px] flex-shrink-0"
-									>
+							<Separator />
+							<div className="overflow-x-auto">
+								<div className="flex gap-4 pb-4">
+									<Card className="border-dashed min-h-[200px] flex flex-col text-balance text-center w-[250px] flex-shrink-0">
 										<CardHeader>
-											<CardTitle>{channel.channelCustomUrl}</CardTitle>
+											<CardTitle>Connect TikTok</CardTitle>
 										</CardHeader>
-										<CardContent>
-											<div className="flex items-center space-x-4 justify-center">
-												<Avatar className="size-20">
-													<AvatarImage
-														src={channel.avatar as string}
-														alt={`${channel.channelCustomUrl} profile`}
-													/>
-													<AvatarFallback>{channel.channelCustomUrl[0]}</AvatarFallback>
-												</Avatar>
-												<div>
-													{channel.error && <p className="text-red-500">{channel.error}</p>}
-												</div>
-											</div>
+										<CardContent className="flex flex-1">
+											<form action={connectTiktokAccount} className="w-full flex items-end">
+												<Button size="sm" className="w-full">
+													<Icons.tiktok className="size-4 mr-2" />
+													Connect TikTok
+												</Button>
+											</form>
 										</CardContent>
-										<CardFooter>
-											<Button variant="destructive" size="sm" className="w-full">
-												Disconnect
-											</Button>
-										</CardFooter>
 									</Card>
-								))}
+									{tiktokChannels.map((channel) => (
+										<Card
+											key={channel.id}
+											className="justify-center min-h-[200px] text-center w-[250px] flex-shrink-0"
+										>
+											<CardHeader>
+												<CardTitle>{channel.channelCustomUrl}</CardTitle>
+											</CardHeader>
+											<CardContent>
+												<div className="flex items-center space-x-4 justify-center">
+													<Avatar className="size-20">
+														<AvatarImage
+															src={channel.avatar as string}
+															alt={`${channel.channelCustomUrl} profile`}
+														/>
+														<AvatarFallback>{channel.channelCustomUrl[0]}</AvatarFallback>
+													</Avatar>
+													<div>
+														{channel.error && <p className="text-red-500">{channel.error}</p>}
+													</div>
+												</div>
+											</CardContent>
+											<CardFooter>
+												<Button variant="destructive" size="sm" className="w-full">
+													Disconnect
+												</Button>
+											</CardFooter>
+										</Card>
+									))}
+								</div>
 							</div>
 						</div>
 					</div>
