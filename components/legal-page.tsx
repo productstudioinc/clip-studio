6833f0e805dbebed94d6067bea2087b1f4584433
gursheyss <rkwarya@gmail.com>
2024-08-01 06:06:39 -0700

    fix: legal page visibility on dark mode

--- components/legal-page.tsx
+++ components/legal-page.tsx
@@ -6,15 +6,15 @@ export default function LegalPage({ page }: { page: Page }) {
 	return (
 		<>
 			<div className="py-20 sm:py-10">
-				<h1 className="mt-5 text-center font-display text-4xl font-bold leading-[1.15] text-black sm:text-5xl sm:leading-[1.15]">
+				<h1 className="mt-5 text-center font-display text-4xl font-bold leading-[1.15] text-primary sm:text-5xl sm:leading-[1.15]">
 					{page.title}
 				</h1>
 				<div className="mx-auto mt-10 w-full max-w-screen-md border-t border-gray-200 pt-10 text-center">
-					<p className="text-gray-500">Last updated: {formatDate(page.updatedAt)}</p>
+					<p className="text-priamry">Last updated: {formatDate(page.updatedAt)}</p>
 				</div>
 			</div>
 
-			<div className="flex flex-col text-left pb-10 pt-10 container prose">
+			<div className="flex flex-col text-left pb-10 pt-10 container prose dark:prose-invert">
 				<MDXContent code={page.mdx} />{' '}
 			</div>
 		</>
