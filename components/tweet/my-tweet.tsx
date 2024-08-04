4d32b54f8b22a6323189eb37b7df334586f7ad72
gursheyss <rkwarya@gmail.com>
2024-08-04 11:15:26 -0700

    fix: tweets show up correctly on renders

--- components/tweet/my-tweet.tsx
+++ components/tweet/my-tweet.tsx
@@ -1,7 +1,7 @@
 import {
 	type TwitterComponents,
-	QuotedTweet,
 	TweetBody,
+	TweetContainer,
 	TweetHeader,
 	TweetMedia,
 	enrichTweet
@@ -16,14 +16,11 @@ type Props = {
 export const MyTweet = ({ tweet: t, components }: Props) => {
 	const tweet = enrichTweet(t);
 	return (
-		<div className="bg-[#15202b] text-white w-full p-6 rounded-2xl">
+		<TweetContainer>
 			<TweetHeader tweet={tweet} components={components} />
-			<div className="text-2xl py-2">
-				<TweetBody tweet={tweet} />
-			</div>
+			<TweetBody tweet={tweet} />
 			{tweet.mediaDetails?.length ? <TweetMedia tweet={tweet} components={components} /> : null}
-			{tweet.quoted_tweet && <QuotedTweet tweet={tweet.quoted_tweet} />}
 			{/* We're not including the `TweetReplies` component that adds the reply button */}
-		</div>
+		</TweetContainer>
 	);
 };
