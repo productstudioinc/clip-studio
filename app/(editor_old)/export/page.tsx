620f3aa8cf72cd23ae1b7f0efab1046dd6526c6a
gursheyss <rkwarya@gmail.com>
2024-08-20 11:36:01 -0700

    chore: remove export page redirecting

--- app/(editor_old)/export/page.tsx
+++ app/(editor_old)/export/page.tsx
@@ -1,13 +1,11 @@
 import { getUser } from '@/actions/auth/user';
 import { fetchUserConnectSocialMediaAccounts } from '@/actions/db/social-media-queries';
-import { redirect } from 'next/navigation';
 import { RenderControls } from './render-component';
 
 export default async function ExportPage() {
 	const { user } = await getUser();
-	if (!user) {
-		redirect('/login');
-	}
-	const { youtubeChannels, tiktokAccounts } = await fetchUserConnectSocialMediaAccounts(user.id);
+	const { youtubeChannels = [], tiktokAccounts = [] } = user
+		? await fetchUserConnectSocialMediaAccounts(user.id)
+		: {};
 	return <RenderControls youtubeChannels={youtubeChannels} tiktokAccounts={tiktokAccounts} />;
 }
