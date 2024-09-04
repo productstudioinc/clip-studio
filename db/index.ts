b0bd6bd16161c29a17f2f76e90268e4e6d6f6525
gursheyss <rkwarya@gmail.com>
2024-09-04 09:12:17 -0700

    rollback db change

--- db/index.ts
+++ db/index.ts
@@ -2,17 +2,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const client = postgres(process.env.DATABASE_URL!, {
-	connect_timeout: 30, // Set a timeout of 10 seconds
-	onnotice: () => {} // Ignore notice messages
-});
-
-let db;
-try {
-	db = drizzle(client, { schema });
-} catch (error) {
-	console.error('Failed to connect to the database:', error);
-	// You might want to handle this error more gracefully in a production environment
-}
-
-export { db };
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle(client, { schema });
