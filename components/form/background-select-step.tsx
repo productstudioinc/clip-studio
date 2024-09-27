55fa00f54267437e39419899f42e5c8f7b0ba987
gursheyss <rkwarya@gmail.com>
2024-09-27 13:50:37 -0700

    feat: background select functionality

--- components/form/background-select-step.tsx
+++ components/form/background-select-step.tsx
@@ -2,13 +2,13 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
-import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { SelectBackgroundWithParts } from '@/db/schema';
-import { VideoProps } from '@/stores/templatestore';
+import { BackgroundTheme, VideoProps } from '@/stores/templatestore';
 import type { FC } from 'react';
 import { UseFormReturn } from 'react-hook-form';
+import { Label } from '../ui/label';
 
 type BackgroundSelectStepProps = {
 	form: UseFormReturn<VideoProps>;
@@ -16,6 +16,14 @@ type BackgroundSelectStepProps = {
 };
 
 export const BackgroundSelectStep: FC<BackgroundSelectStepProps> = ({ form, backgrounds }) => {
+	const handleSelect = (background: SelectBackgroundWithParts) => {
+		form.setValue('backgroundTheme', background.name as BackgroundTheme);
+		form.setValue(
+			'backgroundUrls',
+			background.backgroundParts.map((part) => part.partUrl)
+		);
+	};
+
 	return (
 		<Card>
 			<CardHeader>
@@ -30,27 +38,32 @@ export const BackgroundSelectStep: FC<BackgroundSelectStepProps> = ({ form, back
 							<FormItem>
 								<FormControl>
 									<RadioGroup
-										onValueChange={field.onChange}
-										defaultValue={field.value}
+										onValueChange={(value) => {
+											field.onChange(value);
+											const selectedBackground = backgrounds.find((bg) => bg.name === value);
+											if (selectedBackground) {
+												handleSelect(selectedBackground);
+											}
+										}}
+										value={field.value}
 										className="flex space-x-4"
 									>
 										{backgrounds.map((background) => (
-											<Label
-												key={background.id}
-												className="relative flex-shrink-0 flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
-											>
-												<RadioGroupItem value={background.name} className="sr-only" />
-												<video
-													src={background.previewUrl}
-													width={200}
-													height={150}
-													className="w-[200px] h-[150px] object-cover rounded-t-md"
-													autoPlay
-													loop
-													muted
-												/>
-												<span className="w-full p-2 text-center">{background.name}</span>
-											</Label>
+											<div key={background.id} className="flex-shrink-0">
+												<Label className="relative flex-shrink-0 hover:cursor-pointer flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary">
+													<RadioGroupItem value={background.name} className="sr-only" />
+													<div className="w-[200px]">
+														<video
+															src={background.previewUrl}
+															className="w-full h-[150px] object-cover rounded-md"
+															autoPlay
+															loop
+															muted
+														/>
+														<div className="w-full p-2 text-center">{background.name}</div>
+													</div>
+												</Label>
+											</div>
 										))}
 									</RadioGroup>
 								</FormControl>
