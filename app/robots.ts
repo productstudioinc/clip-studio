2e43fbd1e0a914d9bd229b57e371ca90c725bd97
gursheyss <rkwarya@gmail.com>
2024-09-28 16:24:49 -0700

    add robots.txt and sitemap

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: {
+			userAgent: '*',
+			allow: '/',
+			disallow: ['/api/', '/_next/', '/public/', '/auth/']
+		},
+		sitemap: 'https://clip.studio/sitemap.xml'
+	};
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { allPages } from 'content-collections';
+import { MetadataRoute } from 'next';
+import { headers } from 'next/headers';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	const headersList = headers();
+	let domain = headersList.get('host') as string;
+	let protocol = 'https';
+
+	return [
+		{
+			url: `${protocol}://${domain}`,
+			lastModified: new Date()
+		},
+		...allPages.map((post) => ({
+			url: `${protocol}://${domain}/${post.slug}`,
+			lastModified: new Date()
+		}))
+	];
+}
