7dcebb4ce525ef87f8197ff236625f74a7fc6d43
gursheyss <rkwarya@gmail.com>
2024-09-28 19:22:31 -0700

    update default transcription

--- stores/splitscreentranscription.ts
+++ stores/splitscreentranscription.ts
@@ -2,820 +2,820 @@ export const splitScreenTranscriptionDefault = {
 	text: " There's two main categorical reasons why I say Harris would be better than Trump. The first category is character. Speaking of which, you know, if we're bringing up stuff from the past, we can say she slept with Willie Brown, broke up a family just to get her job. She got 4.5 million votes to get into AG and DA and slept with zero of them. You don't know that though. Do you know that? No. Okay. Absolutely not. So you think that she slept with 4.5 million people to get in her position of power? I'm sure she could have. But the one person that she did was the most important person that she did. Did you just say that she had to sleep with 4.5 million people. My name is Dean Withers. I'm a 19-year-old liberal. I do political debate for a living. And today, I'm surrounded by 20 Trump supporters. My first claim is Donald Trump is a racist. Beep. Beep. Beep. Beep. Yeah, so I don't understand how people get this idea that Trump is racist. You know, my family came from a communist country. They came from Cuba and they came from Mexico. So",
 	chunks: [
 		{
-			text: " There's",
-			timestamp: [0, 0.18]
+			timestamp: [0, 0.18],
+			text: "There's"
 		},
 		{
-			text: ' two',
-			timestamp: [0.18, 0.32]
+			timestamp: [0.18, 0.32],
+			text: 'two'
 		},
 		{
-			text: ' main',
+			text: 'main',
 			timestamp: [0.32, 0.54]
 		},
 		{
 			timestamp: [0.54, 0.96],
-			text: ' categorical'
+			text: 'categorical'
 		},
 		{
-			text: ' reasons',
-			timestamp: [0.96, 1.24]
+			timestamp: [0.96, 1.24],
+			text: 'reasons'
 		},
 		{
 			timestamp: [1.24, 1.56],
-			text: ' why'
+			text: 'why'
 		},
 		{
-			timestamp: [1.56, 1.64],
-			text: ' I'
+			text: 'I',
+			timestamp: [1.56, 1.64]
 		},
 		{
-			text: ' say',
-			timestamp: [1.64, 1.72]
+			timestamp: [1.64, 1.72],
+			text: 'say'
 		},
 		{
-			text: ' Harris',
-			timestamp: [1.72, 2.02]
+			timestamp: [1.72, 2.02],
+			text: 'Harris'
 		},
 		{
-			timestamp: [2.02, 2.2],
-			text: ' would'
+			text: 'would',
+			timestamp: [2.02, 2.2]
 		},
 		{
-			text: ' be',
+			text: 'be',
 			timestamp: [2.2, 2.34]
 		},
 		{
 			timestamp: [2.34, 2.48],
-			text: ' better'
+			text: 'better'
 		},
 		{
-			text: ' than',
+			text: 'than',
 			timestamp: [2.48, 2.64]
 		},
 		{
-			text: ' Trump.',
-			timestamp: [2.64, 3.02]
+			timestamp: [2.64, 3.02],
+			text: 'Trump'
 		},
 		{
 			timestamp: [3.02, 3.36],
-			text: ' The'
+			text: 'The'
 		},
 		{
 			timestamp: [3.36, 3.62],
-			text: ' first'
+			text: 'first'
 		},
 		{
 			timestamp: [3.62, 3.94],
-			text: ' category'
+			text: 'category'
 		},
 		{
-			text: ' is',
-			timestamp: [3.94, 4.22]
+			timestamp: [3.94, 4.22],
+			text: 'is'
 		},
 		{
-			timestamp: [4.22, 4.74],
-			text: ' character.'
+			text: 'character',
+			timestamp: [4.22, 4.74]
 		},
 		{
-			text: ' Speaking',
-			timestamp: [4.74, 5.08]
+			timestamp: [4.74, 5.08],
+			text: 'Speaking'
 		},
 		{
 			timestamp: [5.08, 5.32],
-			text: ' of'
+			text: 'of'
 		},
 		{
-			text: ' which,',
-			timestamp: [5.32, 5.52]
+			timestamp: [5.32, 5.52],
+			text: 'which'
 		},
 		{
 			timestamp: [5.52, 5.6],
-			text: ' you'
+			text: 'you'
 		},
 		{
-			text: ' know,',
-			timestamp: [5.6, 5.7]
+			timestamp: [5.6, 5.7],
+			text: 'know'
 		},
 		{
 			timestamp: [5.7, 5.8],
-			text: ' if'
+			text: 'if'
 		},
 		{
 			timestamp: [5.8, 5.9],
-			text: " we're"
+			text: "we're"
 		},
 		{
 			timestamp: [5.9, 6.02],
-			text: ' bringing'
+			text: 'bringing'
 		},
 		{
-			text: ' up',
+			text: 'up',
 			timestamp: [6.02, 6.18]
 		},
 		{
-			text: ' stuff',
+			text: 'stuff',
 			timestamp: [6.18, 6.34]
 		},
 		{
-			text: ' from',
+			text: 'from',
 			timestamp: [6.34, 6.48]
 		},
 		{
 			timestamp: [6.48, 6.58],
-			text: ' the'
+			text: 'the'
 		},
 		{
-			text: ' past,',
+			text: 'past',
 			timestamp: [6.58, 6.88]
 		},
 		{
 			timestamp: [6.88, 7],
-			text: ' we'
+			text: 'we'
 		},
 		{
-			text: ' can',
+			text: 'can',
 			timestamp: [7, 7.1]
 		},
 		{
-			text: ' say',
-			timestamp: [7.1, 7.28]
+			timestamp: [7.1, 7.28],
+			text: 'say'
 		},
 		{
-			timestamp: [7.28, 7.66],
-			text: ' she'
+			text: 'she',
+			timestamp: [7.28, 7.66]
 		},
 		{
 			timestamp: [7.66, 7.82],
-			text: ' slept'
+			text: 'slept'
 		},
 		{
-			text: ' with',
+			text: 'with',
 			timestamp: [7.82, 7.98]
 		},
 		{
 			timestamp: [7.98, 8.14],
-			text: ' Willie'
+			text: 'Willie'
 		},
 		{
 			timestamp: [8.14, 8.52],
-			text: ' Brown,'
+			text: 'Brown'
 		},
 		{
-			timestamp: [8.52, 8.74],
-			text: ' broke'
+			text: 'broke',
+			timestamp: [8.52, 8.74]
 		},
 		{
-			timestamp: [8.74, 8.9],
-			text: ' up'
+			text: 'up',
+			timestamp: [8.74, 8.9]
 		},
 		{
-			timestamp: [8.9, 8.96],
-			text: ' a'
+			text: 'a',
+			timestamp: [8.9, 8.96]
 		},
 		{
-			timestamp: [8.98, 9.26],
-			text: ' family'
+			text: 'family',
+			timestamp: [8.98, 9.26]
 		},
 		{
 			timestamp: [9.26, 9.62],
-			text: ' just'
+			text: 'just'
 		},
 		{
 			timestamp: [9.62, 9.74],
-			text: ' to'
+			text: 'to'
 		},
 		{
-			text: ' get',
-			timestamp: [9.74, 9.86]
+			timestamp: [9.74, 9.86],
+			text: 'get'
 		},
 		{
-			text: ' her',
-			timestamp: [9.86, 9.92]
+			timestamp: [9.86, 9.92],
+			text: 'her'
 		},
 		{
-			text: ' job.',
-			timestamp: [9.92, 10.26]
+			timestamp: [9.92, 10.26],
+			text: 'job'
 		},
 		{
 			timestamp: [10.26, 10.48],
-			text: ' She'
+			text: 'She'
 		},
 		{
-			text: ' got',
+			text: 'got',
 			timestamp: [10.48, 10.78]
 		},
 		{
-			text: ' 4',
-			timestamp: [10.78, 10.84]
+			timestamp: [10.78, 10.84],
+			text: '4'
 		},
 		{
-			text: '.5',
-			timestamp: [10.84, 11.3]
+			timestamp: [10.84, 11.3],
+			text: '5'
 		},
 		{
 			timestamp: [11.3, 11.6],
-			text: ' million'
+			text: 'million'
 		},
 		{
-			timestamp: [11.6, 11.92],
-			text: ' votes'
+			text: 'votes',
+			timestamp: [11.6, 11.92]
 		},
 		{
 			timestamp: [11.92, 12.18],
-			text: ' to'
+			text: 'to'
 		},
 		{
-			timestamp: [12.18, 12.2],
-			text: ' get'
+			text: 'get',
+			timestamp: [12.18, 12.2]
 		},
 		{
 			timestamp: [12.2, 12.34],
-			text: ' into'
+			text: 'into'
 		},
 		{
 			timestamp: [12.34, 12.66],
-			text: ' AG'
+			text: 'AG'
 		},
 		{
-			text: ' and',
-			timestamp: [12.66, 13.12]
+			timestamp: [12.66, 13.12],
+			text: 'and'
 		},
 		{
 			timestamp: [13.12, 13.38],
-			text: ' DA'
+			text: 'DA'
 		},
 		{
 			timestamp: [13.38, 13.84],
-			text: ' and'
+			text: 'and'
 		},
 		{
 			timestamp: [13.84, 14],
-			text: ' slept'
+			text: 'slept'
 		},
 		{
 			timestamp: [14, 14.16],
-			text: ' with'
+			text: 'with'
 		},
 		{
 			timestamp: [14.16, 14.56],
-			text: ' zero'
+			text: 'zero'
 		},
 		{
 			timestamp: [14.56, 14.74],
-			text: ' of'
+			text: 'of'
 		},
 		{
-			text: ' them.',
+			text: 'them',
 			timestamp: [14.74, 15]
 		},
 		{
 			timestamp: [15, 15.12],
-			text: ' You'
+			text: 'You'
 		},
 		{
-			text: " don't",
+			text: "don't",
 			timestamp: [15.12, 15.28]
 		},
 		{
-			timestamp: [15.28, 15.28],
-			text: ' know'
+			text: 'know',
+			timestamp: [15.28, 15.28]
 		},
 		{
 			timestamp: [15.28, 15.56],
-			text: ' that'
+			text: 'that'
 		},
 		{
 			timestamp: [15.56, 15.7],
-			text: ' though.'
+			text: 'though'
 		},
 		{
-			text: ' Do',
-			timestamp: [15.7, 15.96]
+			timestamp: [15.7, 15.96],
+			text: 'Do'
 		},
 		{
 			timestamp: [15.96, 16.14],
-			text: ' you'
+			text: 'you'
 		},
 		{
 			timestamp: [16.14, 16.14],
-			text: ' know'
+			text: 'know'
 		},
 		{
-			text: ' that?',
-			timestamp: [16.14, 16.42]
+			timestamp: [16.14, 16.42],
+			text: 'that'
 		},
 		{
-			text: ' No.',
-			timestamp: [16.44, 16.92]
+			timestamp: [16.44, 16.92],
+			text: 'No'
 		},
 		{
 			timestamp: [16.92, 17.28],
-			text: ' Okay.'
+			text: 'Okay'
 		},
 		{
-			text: ' Absolutely',
-			timestamp: [17.28, 17.52]
+			timestamp: [17.28, 17.52],
+			text: 'Absolutely'
 		},
 		{
-			timestamp: [17.52, 17.82],
-			text: ' not.'
+			text: 'not',
+			timestamp: [17.52, 17.82]
 		},
 		{
-			text: ' So',
-			timestamp: [17.82, 17.94]
-		},
-		{
-			text: ' you',
-			timestamp: [17.94, 17.94]
+			timestamp: [17.82, 17.94],
+			text: 'So'
 		},
 		{
 			timestamp: [17.94, 17.94],
-			text: ' think'
+			text: 'you'
+		},
+		{
+			timestamp: [17.94, 17.94],
+			text: 'think'
 		},
 		{
 			timestamp: [17.94, 18.06],
-			text: ' that'
+			text: 'that'
 		},
 		{
 			timestamp: [18.06, 18.22],
-			text: ' she'
+			text: 'she'
 		},
 		{
-			text: ' slept',
-			timestamp: [18.22, 18.36]
+			timestamp: [18.22, 18.36],
+			text: 'slept'
 		},
 		{
 			timestamp: [18.36, 18.56],
-			text: ' with'
+			text: 'with'
 		},
 		{
-			timestamp: [18.56, 18.78],
-			text: ' 4'
+			text: '4',
+			timestamp: [18.56, 18.78]
 		},
 		{
 			timestamp: [18.78, 19.26],
-			text: '.5'
+			text: '5'
 		},
 		{
 			timestamp: [19.26, 19.52],
-			text: ' million'
+			text: 'million'
 		},
 		{
-			text: ' people',
-			timestamp: [19.52, 19.82]
+			timestamp: [19.52, 19.82],
+			text: 'people'
 		},
 		{
-			text: ' to',
-			timestamp: [19.82, 19.98]
+			timestamp: [19.82, 19.98],
+			text: 'to'
 		},
 		{
-			text: ' get',
-			timestamp: [19.98, 20.06]
+			timestamp: [19.98, 20.06],
+			text: 'get'
 		},
 		{
 			timestamp: [20.06, 20.06],
-			text: ' in'
+			text: 'in'
 		},
 		{
 			timestamp: [20.06, 20.18],
-			text: ' her'
+			text: 'her'
 		},
 		{
-			text: ' position',
-			timestamp: [20.18, 20.4]
+			timestamp: [20.18, 20.4],
+			text: 'position'
 		},
 		{
-			timestamp: [20.4, 20.48],
-			text: ' of'
+			text: 'of',
+			timestamp: [20.4, 20.48]
 		},
 		{
-			timestamp: [20.48, 20.96],
-			text: ' power?'
+			text: 'power',
+			timestamp: [20.48, 20.96]
 		},
 		{
 			timestamp: [20.96, 20.98],
-			text: " I'm"
+			text: "I'm"
 		},
 		{
-			text: ' sure',
+			text: 'sure',
+			timestamp: [20.98, 20.98]
+		},
+		{
+			text: 'she',
+			timestamp: [20.98, 20.98]
+		},
+		{
+			text: 'could',
 			timestamp: [20.98, 20.98]
 		},
 		{
 			timestamp: [20.98, 20.98],
-			text: ' she'
+			text: 'have'
 		},
 		{
-			text: ' could',
-			timestamp: [20.98, 20.98]
+			timestamp: [20.98, 21],
+			text: 'But'
 		},
 		{
-			timestamp: [20.98, 20.98],
-			text: ' have.'
+			timestamp: [21, 21.22],
+			text: 'the'
 		},
 		{
-			text: ' But',
-			timestamp: [20.98, 21]
+			text: 'one',
+			timestamp: [21.22, 21.48]
 		},
 		{
-			text: ' the',
-			timestamp: [21, 21.22]
-		},
-		{
-			timestamp: [21.22, 21.48],
-			text: ' one'
-		},
-		{
-			text: ' person',
-			timestamp: [21.48, 21.68]
+			timestamp: [21.48, 21.68],
+			text: 'person'
 		},
 		{
 			timestamp: [21.68, 21.86],
-			text: ' that'
+			text: 'that'
 		},
 		{
-			text: ' she',
-			timestamp: [21.86, 22.04]
+			timestamp: [21.86, 22.04],
+			text: 'she'
 		},
 		{
-			timestamp: [22.04, 22.18],
-			text: ' did'
+			text: 'did',
+			timestamp: [22.04, 22.18]
 		},
 		{
 			timestamp: [22.18, 22.56],
-			text: ' was'
+			text: 'was'
 		},
 		{
-			timestamp: [22.56, 22.78],
-			text: ' the'
+			text: 'the',
+			timestamp: [22.56, 22.78]
 		},
 		{
 			timestamp: [22.78, 23],
-			text: ' most'
+			text: 'most'
 		},
 		{
 			timestamp: [23, 23.38],
-			text: ' important'
+			text: 'important'
 		},
 		{
-			text: ' person',
-			timestamp: [23.38, 23.68]
+			timestamp: [23.38, 23.68],
+			text: 'person'
 		},
 		{
 			timestamp: [23.68, 23.74],
-			text: ' that'
-		},
-		{
-			text: ' she',
-			timestamp: [23.74, 23.74]
+			text: 'that'
 		},
 		{
 			timestamp: [23.74, 23.74],
-			text: ' did.'
+			text: 'she'
 		},
 		{
-			text: ' Did',
-			timestamp: [23.74, 23.78]
+			timestamp: [23.74, 23.74],
+			text: 'did'
 		},
 		{
-			text: ' you',
-			timestamp: [23.78, 23.84]
+			timestamp: [23.74, 23.78],
+			text: 'Did'
+		},
+		{
+			timestamp: [23.78, 23.84],
+			text: 'you'
 		},
 		{
 			timestamp: [23.84, 24.1],
-			text: ' just'
+			text: 'just'
 		},
 		{
-			text: ' say',
-			timestamp: [24.1, 24.26]
+			timestamp: [24.1, 24.26],
+			text: 'say'
 		},
 		{
 			timestamp: [24.26, 24.42],
-			text: ' that'
+			text: 'that'
 		},
 		{
 			timestamp: [24.42, 24.6],
-			text: ' she'
+			text: 'she'
 		},
 		{
-			text: ' had',
+			text: 'had',
 			timestamp: [24.6, 24.8]
 		},
 		{
-			timestamp: [24.8, 24.92],
-			text: ' to'
+			text: 'to',
+			timestamp: [24.8, 24.92]
 		},
 		{
-			text: ' sleep',
-			timestamp: [24.92, 25.18]
+			timestamp: [24.92, 25.18],
+			text: 'sleep'
 		},
 		{
 			timestamp: [25.18, 25.32],
-			text: ' with'
+			text: 'with'
 		},
 		{
 			timestamp: [25.32, 25.56],
-			text: ' 4'
+			text: '4'
 		},
 		{
-			text: '.5',
-			timestamp: [25.56, 26.02]
+			timestamp: [25.56, 26.02],
+			text: '5'
 		},
 		{
-			timestamp: [26.02, 26.44],
-			text: ' million'
+			text: 'million',
+			timestamp: [26.02, 26.44]
 		},
 		{
-			text: ' people.',
+			text: 'people',
 			timestamp: [26.44, 26.54]
 		},
 		{
-			text: ' My',
+			text: 'My',
 			timestamp: [26.54, 27.3]
 		},
 		{
-			text: ' name',
-			timestamp: [27.3, 27.5]
+			timestamp: [27.3, 27.5],
+			text: 'name'
 		},
 		{
-			timestamp: [27.5, 27.62],
-			text: ' is'
+			text: 'is',
+			timestamp: [27.5, 27.62]
 		},
 		{
-			text: ' Dean',
+			text: 'Dean',
 			timestamp: [27.62, 27.78]
 		},
 		{
-			timestamp: [27.78, 28.1],
-			text: ' Withers.'
+			text: 'Withers',
+			timestamp: [27.78, 28.1]
 		},
 		{
-			text: " I'm",
-			timestamp: [28.1, 28.3]
+			timestamp: [28.1, 28.3],
+			text: "I'm"
 		},
 		{
-			text: ' a',
-			timestamp: [28.3, 28.32]
+			timestamp: [28.3, 28.32],
+			text: 'a'
 		},
 		{
-			timestamp: [28.32, 28.66],
-			text: ' 19'
+			text: '19',
+			timestamp: [28.32, 28.66]
 		},
 		{
 			timestamp: [28.66, 28.86],
-			text: '-year'
+			text: 'year'
 		},
 		{
 			timestamp: [28.86, 28.96],
-			text: '-old'
+			text: 'old'
 		},
 		{
 			timestamp: [28.96, 29.26],
-			text: ' liberal.'
+			text: 'liberal'
 		},
 		{
-			text: ' I',
-			timestamp: [29.26, 29.46]
+			timestamp: [29.26, 29.46],
+			text: 'I'
 		},
 		{
 			timestamp: [29.46, 29.58],
-			text: ' do'
+			text: 'do'
 		},
 		{
 			timestamp: [29.58, 29.88],
-			text: ' political'
+			text: 'political'
 		},
 		{
 			timestamp: [29.88, 30.14],
-			text: ' debate'
+			text: 'debate'
 		},
 		{
 			timestamp: [30.14, 30.34],
-			text: ' for'
+			text: 'for'
 		},
 		{
 			timestamp: [30.34, 30.4],
-			text: ' a'
+			text: 'a'
 		},
 		{
-			text: ' living.',
+			text: 'living',
 			timestamp: [30.4, 30.7]
 		},
 		{
-			timestamp: [30.7, 30.84],
-			text: ' And'
+			text: 'And',
+			timestamp: [30.7, 30.84]
 		},
 		{
 			timestamp: [30.84, 31.18],
-			text: ' today,'
+			text: 'today'
 		},
 		{
-			text: " I'm",
+			text: "I'm",
 			timestamp: [31.18, 31.56]
 		},
 		{
 			timestamp: [31.56, 31.78],
-			text: ' surrounded'
+			text: 'surrounded'
 		},
 		{
-			text: ' by',
+			text: 'by',
 			timestamp: [31.78, 32.02]
 		},
 		{
 			timestamp: [32.02, 32.28],
-			text: ' 20'
+			text: '20'
 		},
 		{
-			timestamp: [32.26, 32.54],
-			text: ' Trump'
+			text: 'Trump',
+			timestamp: [32.26, 32.54]
 		},
 		{
-			text: ' supporters.',
-			timestamp: [32.54, 33.18]
+			timestamp: [32.54, 33.18],
+			text: 'supporters'
 		},
 		{
-			text: ' My',
-			timestamp: [33.18, 37.44]
+			timestamp: [33.18, 37.44],
+			text: 'My'
 		},
 		{
 			timestamp: [37.44, 37.76],
-			text: ' first'
+			text: 'first'
 		},
 		{
 			timestamp: [37.76, 38],
-			text: ' claim'
+			text: 'claim'
 		},
 		{
-			text: ' is',
-			timestamp: [38, 38.22]
+			timestamp: [38, 38.22],
+			text: 'is'
 		},
 		{
-			timestamp: [38.22, 38.62],
-			text: ' Donald'
+			text: 'Donald',
+			timestamp: [38.22, 38.62]
 		},
 		{
-			text: ' Trump',
-			timestamp: [38.62, 38.94]
+			timestamp: [38.62, 38.94],
+			text: 'Trump'
 		},
 		{
 			timestamp: [38.94, 39.12],
-			text: ' is'
+			text: 'is'
 		},
 		{
 			timestamp: [39.12, 39.2],
-			text: ' a'
+			text: 'a'
 		},
 		{
 			timestamp: [39.2, 39.74],
-			text: ' racist.'
+			text: 'racist'
 		},
 		{
-			text: ' Beep.',
+			text: 'Beep',
 			timestamp: [43.2, 43.88]
 		},
 		{
-			text: ' Beep.',
-			timestamp: [43.88, 44.64]
+			timestamp: [43.88, 44.64],
+			text: 'Beep'
 		},
 		{
 			timestamp: [44.64, 45.6],
-			text: ' Beep.'
+			text: 'Beep'
 		},
 		{
-			timestamp: [45.6, 46.16],
-			text: ' Beep.'
+			text: 'Beep',
+			timestamp: [45.6, 46.16]
 		},
 		{
-			text: ' Yeah,',
-			timestamp: [50.46, 51.22]
+			timestamp: [50.46, 51.22],
+			text: 'Yeah'
 		},
 		{
-			timestamp: [51.22, 51.32],
-			text: ' so'
+			text: 'so',
+			timestamp: [51.22, 51.32]
 		},
 		{
-			timestamp: [51.32, 51.52],
-			text: ' I'
+			text: 'I',
+			timestamp: [51.32, 51.52]
 		},
 		{
 			timestamp: [51.52, 51.7],
-			text: " don't"
+			text: "don't"
 		},
 		{
 			timestamp: [51.7, 51.98],
-			text: ' understand'
+			text: 'understand'
 		},
 		{
-			text: ' how',
+			text: 'how',
 			timestamp: [51.98, 52.26]
 		},
 		{
 			timestamp: [52.26, 52.52],
-			text: ' people'
+			text: 'people'
 		},
 		{
 			timestamp: [52.52, 52.76],
-			text: ' get'
+			text: 'get'
 		},
 		{
 			timestamp: [52.76, 52.98],
-			text: ' this'
+			text: 'this'
 		},
 		{
-			text: ' idea',
-			timestamp: [52.98, 53.4]
+			timestamp: [52.98, 53.4],
+			text: 'idea'
 		},
 		{
-			text: ' that',
-			timestamp: [53.4, 53.84]
+			timestamp: [53.4, 53.84],
+			text: 'that'
 		},
 		{
-			text: ' Trump',
-			timestamp: [53.84, 54.2]
+			timestamp: [53.84, 54.2],
+			text: 'Trump'
 		},
 		{
-			text: ' is',
-			timestamp: [54.2, 54.4]
+			timestamp: [54.2, 54.4],
+			text: 'is'
 		},
 		{
 			timestamp: [54.4, 55.08],
-			text: ' racist.'
+			text: 'racist'
 		},
 		{
 			timestamp: [55.08, 55.16],
-			text: ' You'
+			text: 'You'
 		},
 		{
 			timestamp: [55.16, 55.24],
-			text: ' know,'
+			text: 'know'
 		},
 		{
-			timestamp: [55.24, 55.32],
-			text: ' my'
+			text: 'my',
+			timestamp: [55.24, 55.32]
 		},
 		{
 			timestamp: [55.32, 55.58],
-			text: ' family'
+			text: 'family'
 		},
 		{
 			timestamp: [55.58, 55.92],
-			text: ' came'
+			text: 'came'
 		},
 		{
-			text: ' from',
-			timestamp: [55.92, 56.26]
+			timestamp: [55.92, 56.26],
+			text: 'from'
 		},
 		{
-			text: ' a',
-			timestamp: [56.26, 56.38]
+			timestamp: [56.26, 56.38],
+			text: 'a'
 		},
 		{
-			text: ' communist',
+			text: 'communist',
 			timestamp: [56.38, 56.6]
 		},
 		{
 			timestamp: [56.6, 57.06],
-			text: ' country.'
+			text: 'country'
 		},
 		{
 			timestamp: [57.06, 57.12],
-			text: ' They'
+			text: 'They'
 		},
 		{
-			timestamp: [57.12, 57.26],
-			text: ' came'
+			text: 'came',
+			timestamp: [57.12, 57.26]
 		},
 		{
 			timestamp: [57.26, 57.42],
-			text: ' from'
+			text: 'from'
 		},
 		{
-			text: ' Cuba',
-			timestamp: [57.42, 57.74]
+			timestamp: [57.42, 57.74],
+			text: 'Cuba'
 		},
 		{
 			timestamp: [57.74, 58.42],
-			text: ' and'
+			text: 'and'
 		},
 		{
-			text: ' they',
-			timestamp: [58.42, 58.54]
+			timestamp: [58.42, 58.54],
+			text: 'they'
 		},
 		{
 			timestamp: [58.54, 58.68],
-			text: ' came'
+			text: 'came'
 		},
 		{
-			text: ' from',
+			text: 'from',
 			timestamp: [58.68, 58.86]
 		},
 		{
-			text: ' Mexico.',
-			timestamp: [58.86, 59.32]
+			timestamp: [58.86, 59.32],
+			text: 'Mexico'
 		},
 		{
-			text: ' So',
-			timestamp: [59.32, 59.76]
+			timestamp: [59.32, 59.76],
+			text: 'So'
 		}
 	]
 };
