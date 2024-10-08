78bacecbe140906c13b052544d0ba5c989f3f030
Dillion Verma <hello@dillion.io>
2024-10-08 04:57:01 -0400

    fix: noindex for admin page

--- app/(app)/admin/layout.tsx
+++ app/(app)/admin/layout.tsx
@@ -3,6 +3,10 @@ import { redirect } from 'next/navigation'
 import { getUser } from '@/actions/auth/user'
 import { isAdmin } from '@/actions/db/admin-queries'
 
+export const metadata: Metadata = {
+  robots: 'noindex, nofollow'
+}
+
 export default async function AdminLayout({
   children
 }: {
