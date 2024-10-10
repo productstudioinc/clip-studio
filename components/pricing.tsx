a7ebc973d982db689a4fa0fe6abe6efff1677350
gursheyss <rkwarya@gmail.com>
2024-10-10 16:22:40 -0700

    add credit cost for ai stuff

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -157,6 +157,14 @@ export default function Pricing({
                 <span className="flex">20 credits</span>
               </li>
               <li className="flex items-center gap-3 text-sm font-medium text-black dark:text-white">
+                <CheckIcon className="h-5 w-5 shrink-0 rounded-full bg-green-600 p-[2px] text-white dark:text-white" />
+                <span className="flex">1 credit = 1 script generation</span>
+              </li>
+              <li className="flex items-center gap-3 text-sm font-medium text-black dark:text-white">
+                <CheckIcon className="h-5 w-5 shrink-0 rounded-full bg-green-600 p-[2px] text-white dark:text-white" />
+                <span className="flex">10 credits = 1 AI Image</span>
+              </li>
+              <li className="flex items-center gap-3 text-sm font-medium text-black dark:text-white">
                 <div className="h-5 w-5 shrink-0 rounded-full bg-red-600 p-[2px] flex items-center justify-center">
                   <span className="text-white font-bold">×</span>
                 </div>
@@ -282,6 +290,9 @@ export default function Pricing({
                         {(!isLoading ||
                           (isLoading && id !== currentPrice?.id)) && (
                           <p>Upgrade</p>
+                        )}
+                        {isLoading && id === currentPrice?.id && (
+                          <p>Upgrading...</p>
                         )}
                         {isLoading && id === currentPrice?.id && (
                           <p>Upgrading...</p>
