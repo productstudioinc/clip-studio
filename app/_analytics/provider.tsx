b83c75663fb5d1d6b15ac6b3fe5ba53ba120c89f
Dillion Verma <hello@dillion.io>
2024-11-21 18:49:28 -0500

    fix: identify all users posthog

--- app/_analytics/provider.tsx
+++ app/_analytics/provider.tsx
@@ -11,8 +11,6 @@ if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: '/ingest',
     ui_host: 'https://us.posthog.com',
-    person_profiles: 'identified_only',
-    capture_pageview: false,
     capture_pageleave: true // Enable pageleave capture
   })
 }
