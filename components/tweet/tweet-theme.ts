d6a5146dbe36a3ce7325e9c74793010d449a45cc
gursheyss <rkwarya@gmail.com>
2024-11-27 17:40:27 -0800

    update tweet styling

--- components/tweet/tweet-theme.ts
+++ components/tweet/tweet-theme.ts
@@ -1,34 +1,18 @@
 export const tweetTheme = {
   light: {
     background: '#FFFFFF',
-    border: 'rgb(207, 217, 222)',
-    text: 'rgb(15, 20, 25)',
-    textSecondary: 'rgb(83, 100, 113)',
-    bluePrimary: 'rgb(29, 155, 240)',
-    bluePrimaryHover: 'rgb(26, 140, 216)',
-    blueSecondary: 'rgb(0, 111, 214)',
-    blueSecondaryHover: 'rgba(0, 111, 214, 0.1)',
-    redPrimary: 'rgb(249, 24, 128)',
-    redPrimaryHover: 'rgba(249, 24, 128, 0.1)',
-    greenPrimary: 'rgb(0, 186, 124)',
-    greenPrimaryHover: 'rgba(0, 186, 124, 0.1)',
-    verifiedOld: 'rgb(130, 154, 171)',
-    verifiedBlue: 'rgb(29, 155, 240)'
+    text: '#0F1419',
+    textSecondary: '#536471',
+    border: '#EFF3F4',
+    bluePrimary: '#1D9BF0',
+    redPrimary: '#F91880'
   },
   dark: {
-    background: 'rgb(21, 32, 43)',
-    border: 'rgb(66, 83, 100)',
-    text: 'rgb(247, 249, 249)',
-    textSecondary: 'rgb(139, 152, 165)',
-    bluePrimary: 'rgb(29, 155, 240)',
-    bluePrimaryHover: 'rgb(26, 140, 216)',
-    blueSecondary: 'rgb(107, 201, 251)',
-    blueSecondaryHover: 'rgba(107, 201, 251, 0.1)',
-    redPrimary: 'rgb(249, 24, 128)',
-    redPrimaryHover: 'rgba(249, 24, 128, 0.1)',
-    greenPrimary: 'rgb(0, 186, 124)',
-    greenPrimaryHover: 'rgba(0, 186, 124, 0.1)',
-    verifiedOld: 'rgb(130, 154, 171)',
-    verifiedBlue: '#FFFFFF'
+    background: '#15202B',
+    text: '#E7E9EA',
+    textSecondary: '#71767B',
+    border: '#2F3336',
+    bluePrimary: '#1D9BF0',
+    redPrimary: '#F91880'
   }
 }
