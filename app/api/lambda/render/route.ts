cd78643c63e35e5c887d87a5269f90f508d3bb33
gursheyss <rkwarya@gmail.com>
2024-12-08 10:57:29 -0800

    increase delayrender timeout

--- app/api/lambda/render/route.ts
+++ app/api/lambda/render/route.ts
@@ -104,6 +104,7 @@ export const POST = executeApi<RenderMediaOnLambdaOutput, typeof RenderRequest>(
         memorySizeInMb: RAM,
         timeoutInSeconds: TIMEOUT
       }),
+      timeoutInMilliseconds: 180000, // 3 minutes
       region: REGION as AwsRegion,
       serveUrl: SITE_NAME,
       composition: body.id,
