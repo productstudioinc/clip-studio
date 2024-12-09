51f64dd11806024dfcdc5b5db9e83879edeb4044
Dillion Verma <hello@dillion.io>
2024-12-09 03:57:21 -0500

    fix: succes page stle fix

--- app/(app)/success/page.tsx
+++ app/(app)/success/page.tsx
@@ -17,27 +17,29 @@ export default async function SuccessPage({
   searchParams: { session_id: string }
 }) {
   const sessionId = searchParams?.session_id
-  if (!sessionId) {
-    throw new Error(
-      'No session ID found. Please contact support support@clip.studio'
-    )
+  let session = null
+
+  if (sessionId) {
+    session = await stripe.checkout.sessions.retrieve(sessionId)
   }
-  const session = await stripe.checkout.sessions.retrieve(sessionId)
+
   return (
     <section>
       <Confetti />
-      <FacebookTrackOnRender
-        event={{
-          event_name: 'Subscribe',
-          event_id: session.id,
-          custom_data: {
-            value: session.amount_total ? session.amount_total / 100 : 0,
-            currency: session.currency || 'USD'
-          }
-        }}
-      />
+      {session && (
+        <FacebookTrackOnRender
+          event={{
+            event_name: 'Subscribe',
+            event_id: session.id,
+            custom_data: {
+              value: session.amount_total ? session.amount_total / 100 : 0,
+              currency: session.currency || 'USD'
+            }
+          }}
+        />
+      )}
       <div className="container mx-auto flex min-h-[calc(100vh-8rem)] items-center px-6 py-12">
-        <div className="mx-auto flex max-w-lg flex-col items-center text-center">
+        <div className="mx-auto flex max-w-lg flex-col items-center justify-center text-center">
           <p className="rounded-full bg-green-50 p-3 text-sm font-medium dark:bg-green-800">
             <Check className="h-6 w-6 text-green-600 dark:text-green-300" />
           </p>
@@ -45,7 +47,7 @@ export default async function SuccessPage({
             Welcome to Clip Studio!
           </h1>
 
-          <div className="group mt-6 flex w-full shrink-0 items-center gap-x-3 sm:w-auto">
+          <div className="group mt-6 flex w-full shrink-0 items-center justify-center gap-x-3 sm:w-auto">
             <Link href="/" className={buttonVariants({ variant: 'default' })}>
               <Clapperboard className="mr-2 h-4 w-4" />
               <span>Let&apos;s get started</span>
