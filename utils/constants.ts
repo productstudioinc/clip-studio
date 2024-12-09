80d9cecc3bb1699be90d478b33c85d4907049968
Dillion Verma <hello@dillion.io>
2024-12-09 00:44:13 -0500

    fix: add comment

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
 export const CREDIT_CONVERSIONS = {
-  EXPORT_SECONDS: 5,
+  EXPORT_SECONDS: 5, // 1 credit per 5 seconds of video (Math.ceil(durationinframes / 30 / 5))
   VOICEOVER_CHARACTERS: 100,
   TRANSCRIBE_SECONDS: 10,
   IMAGE_GENERATION: 10, // Now this correctly represents 10 credits per image
